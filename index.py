cc5ca567cc79e227b64932f55a44d862a9b64af1
dlucianop <3112dlp@gmail.com>
2024-02-16 17:50:35 -0600

    La funcion ya puede separar la cuenta en digitos, falta convertir los digitos en numeros o en numeros reconocibles o no irreconocibles

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 import os
 import re
+from numeros import representaciones
 
 def index():
     ventana = tk.Tk()
@@ -21,22 +22,37 @@ def openFile():
 def conversion_first_h(archivo):
     with open(archivo, 'r') as file:
         num_cuenta = []
+        indice = 0
         for linea in file:
-            num_cuenta.append(re.split(r'(\s+)', linea))
-            
-            if len(num_cuenta) == 4:
-                print(num_cuenta)
-                numeration(num_cuenta)
+            indice += 1
+            if indice % 4 == 0:
+                continue
+            num_cuenta.append(linea)
+            if len(num_cuenta) == 3:
+                numeros = encontrar_coincidencia(num_cuenta, representaciones)
                 num_cuenta = []
+                print(numeros)
+            print()
+            print()
+                
+    print("Done")
 
-def numeration(cuenta):
-    """
-    [0][0] [0][1] [0][2]
-    [1][0] [1][1] [1][2]
-    [2][0] [2][1] [2][2]"""
-    for fila, lista in enumerate(cuenta):
-        for columna, elemento in enumerate(lista):
-            print(f"Elemento '{elemento}' en la posición ({fila}, {columna})")
+def encontrar_coincidencia(num_cuenta, representaciones):
+    digitos = [[] for _ in range(9)]
+    for fila in num_cuenta:
+        grupo_actual = []
+        for indice, columna in enumerate(fila):
+            grupo_actual.append(columna)
+            if (indice + 1) % 4 == 0:
+                matriz_indice = (indice + 1) // 4 - 1
+                digitos[matriz_indice].append(grupo_actual)
+                grupo_actual = [] 
+
+    for i, matriz in enumerate(digitos, 1):
+        print(f"Digito {i}:")
+        for grupo in matriz:
+            print(''.join(grupo))
+        print()
 
 def validation_second_h():
     pass
