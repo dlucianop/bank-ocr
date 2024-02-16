dbdf84e028c0e02cb51db1b2bd42ff3148a42831
dlucianop <3112dlp@gmail.com>
2024-02-15 19:58:04 -0600

    Se agrego el index y los casos de prueba.

---
+++ index.py
@@ -0,0 +1,16 @@
+"""
+ _
+|_|
+|_|
+
+"""
+
+
+"""La maquina escanea los documentos en papel y produce un archivo con la cantidad de entradas 
+cada entrada tiene 4 lineas y cada linea 27 caracteres 
+las primera 3 lineas tienen una entrada con un numero de cuenta como el de arriba pipes y guiones bajos
+la cuarta esta en blanco
+cada no de cuenta tiene que tener 9 digitios
+de rango 0-9, un archivo normal contiene alrededor de 500 entradas
+
+TOMA EL ARCHIVO y conviertelos en numeros de cuenta reales"""
